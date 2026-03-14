7308828d6d8b927bc1f7c08c867a9ec4ca7cf3c9
yedohnahane-cloud <yedohnahane@gmail.com>
2026-03-14 19:49:02 +0100

    ajout ressources et page quest-rep

--- Back-end/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Exemple de variables (à remplacer plus tard)
-SECRET_KEY = "ma_cle_secrete"
-DB_HOST = "localhost"
-DB_NAME = "entraide_db"
-DB_USER = "postgres"
-DB_PASSWORD = "ton_mot_de_passe"
