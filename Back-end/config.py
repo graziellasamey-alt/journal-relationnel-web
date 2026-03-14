bcc1fa54b81657f185bee30ab0bd4b8e4d7337d3
yedohnahane-cloud <yedohnahane@gmail.com>
2026-03-14 15:05:03 +0100

    ajout de fichiers

---
+++ Back-end/config.py
@@ -0,0 +1,8 @@
+import os
+
+# Exemple de variables (à remplacer plus tard)
+SECRET_KEY = "ma_cle_secrete"
+DB_HOST = "localhost"
+DB_NAME = "entraide_db"
+DB_USER = "postgres"
+DB_PASSWORD = "ton_mot_de_passe"
