3e6483733e20dbb79994c85cff124ad40fe46832
yedohnahane-cloud <yedohnahane@gmail.com>
2026-03-18 23:25:15 +0100

    nouvelles modifications et ajout de event.js

---
+++ Front-end/JAVASCRIPT-PRINCIPAL/event.js
@@ -0,0 +1,43 @@
+const openBtn = document.getElementById("openForm");
+const formBox = document.getElementById("formBox");
+const addBtn = document.getElementById("addBtn");
+const list = document.getElementById("resourcesList");
+
+openBtn.addEventListener("click", function () {
+    formBox.classList.toggle("hidden");
+});
+
+addBtn.addEventListener("click", function () {
+    const titre = document.getElementById("titre").value.trim();
+    const matiere = document.getElementById("matiere").value.trim();
+    const fichierInput = document.getElementById("fichier");
+    const fichier = fichierInput.files[0];
+
+    if (titre === "" || matiere === "" || !fichier) {
+        alert("Remplis le titre, la matière et choisis un fichier.");
+        return;
+    }
+
+    const nomFichier = fichier.name;
+
+    const div = document.createElement("div");
+    div.className = "mini-card";
+    div.innerHTML = `
+        <div class="mini-title-row">
+            <div class="mini-title">${titre}</div>
+        </div>
+
+        <div class="mini-meta">
+            <span class="tag tag-green">Nouveau</span>
+            <span class="tag tag-blue">${matiere}</span>
+            <span class="tag tag-gray">${nomFichier}</span>
+        </div>
+    `;
+
+    list.prepend(div);
+
+    document.getElementById("titre").value = "";
+    document.getElementById("matiere").value = "";
+    fichierInput.value = "";
+    formBox.classList.add("hidden");
+});
