a8ff4d28f60ff579188683d69d214d81c6bbe1f9
Graziella Keli Samey <graziellasamey@gmail.com>
2026-03-24 17:50:11 +0100

    mise a jour de app.py ajout des api de questions et ressources

--- Back-end/app.py
+++ Back-end/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, session, request, Response, redirect, url_for,render_template, abort 
 from models.user_model import create_users_table
 from routes.auth_routes import auth_bp
+from routes.question_routes import question_bp
+from routes.ressource_routes import resource_bp
 import secrets
 from functools import wraps
 from models.db import init_db
@@ -17,6 +19,10 @@ def login_required(f):
             abort(401)
         return f(*args, **kwrags)
     return wrapper
+
+app.register_blueprint(auth_bp, url_prefix="/auth")
+app.register_blueprint(question_bp, url_prefix="/questions")
+app.register_blueprint(resource_bp, url_prefix="/resources")
 
 
 init_db()
